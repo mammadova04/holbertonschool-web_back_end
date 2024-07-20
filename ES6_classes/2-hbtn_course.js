28dcf912348f616595ac3be911e3a6ce46841d5f
mammadova04 <nmammadova04@gmail.com>
2024-07-20 14:04:04 +0000

    fix

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,25 +1,25 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = this._validateName(name);
-    this._length = this._validateLength(length);
-    this._students = this._validateStudents(students);
+    this._name = HolbertonCourse._validateName(name);
+    this._length = HolbertonCourse._validateLength(length);
+    this._students = HolbertonCourse._validateStudents(students);
   }
 
-  _validateName(name) {
+  static _validateName(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     return name;
   }
 
-  _validateLength(length) {
+  static _validateLength(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     return length;
   }
 
-  _validateStudents(students) {
+  static _validateStudents(students) {
     if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
@@ -31,7 +31,7 @@ class HolbertonCourse {
   }
 
   set name(name) {
-    this._name = this._validateName(name);
+    this._name = HolbertonCourse._validateName(name);
   }
 
   get length() {
@@ -39,7 +39,7 @@ class HolbertonCourse {
   }
 
   set length(length) {
-    this._length = this._validateLength(length);
+    this._length = HolbertonCourse._validateLength(length);
   }
 
   get students() {
@@ -47,7 +47,7 @@ class HolbertonCourse {
   }
 
   set students(students) {
-    this._students = this._validateStudents(students);
+    this._students = HolbertonCourse._validateStudents(students);
   }
 }
 

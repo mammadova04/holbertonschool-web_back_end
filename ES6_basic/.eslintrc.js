8a3a33e2da15d4bf2d28ca93f7428f2502079f3a
mammadova04 <nmammadova04@gmail.com>
2024-07-20 09:19:41 +0000

    js

---
+++ ES6_basic/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+        packageDir: ['./', './src'],
+      },
+    ],
+  },
+};

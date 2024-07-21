a39bfa754b03cdb5c7c2ac66b56f1ff027121bb7
mammadova04 <nmammadova04@gmail.com>
2024-07-21 08:10:49 +0000

    fix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,28 +1,17 @@
+/* eslint-disable */
 export default class Building {
-  constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
+    constructor(sqft) {
+      this._sqft = sqft;
+      this.evacuationWarningMessage();
     }
-    this._sqft = sqft;
 
-    if (this.constructor !== Building && typeof this.evacuationWarningMessage === 'undefined') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    get sqft() {
+      return this._sqft;
+    }
+
+    evacuationWarningMessage() {
+      if (this.constructor !== Building) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
   }
-
-  get sqft() {
-    return this._sqft;
-  }
-
-  set sqft(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-    this._sqft = value;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-}

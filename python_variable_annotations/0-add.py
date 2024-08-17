477509a9bd065fc01e233de68f027b0cec338e46
mammadova04 <nmammadova04@gmail.com>
2024-08-17 09:42:47 +0000

    Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Annotating variables and function with float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function adds two floats and returns float
+    """
+    return a + b

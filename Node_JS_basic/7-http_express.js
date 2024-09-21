47d5920995982a5e9ce4dcfed206861c9ce1ad5a
mammadova04 <nmammadova04@gmail.com>
2024-09-21 13:09:38 +0400

    some edits

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -7,18 +7,18 @@ const app = express();
 module.exports = app;
 
 app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.send('Hello Holberton School!');
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-  countStudents(database).then((data) => {
-    res.end(data.join('\n'));
-  }).catch((error) => {
-    res.end(`${error.message}`);
-  });
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('This is the list of our students\n');
+    countStudents(database).then((data) => {
+        res.end(data.join('\n'));
+    }).catch((error) => {
+        res.end(`${error.message}`);
+    });
 });
 
 app.listen(port);
